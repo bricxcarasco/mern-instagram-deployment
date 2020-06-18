1d9be15a115122b8700edf4aad67ca7dfab6eafc
Bricx Carasco <bricxcarasco.gss@gmail.com>
2020-06-18 14:40:46 +0800

    Fix config keys path in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-const { MONGOURI } = require('./keys');
+const { MONGOURI } = require('./config/keys');
 
 const app = express();
 const PORT =  process.env.PORT || 5000;
