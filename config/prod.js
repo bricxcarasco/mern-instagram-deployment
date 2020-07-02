10be74ae80172725f2c44a369a136c51d6ab415d
Bricx Carasco <bricxraincarasco21@gmail.com>
2020-07-02 16:52:53 +0800

    Add sendgrid api env getter in production config

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,8 @@
+const { SENDGRID_APIKEY } = require("./dev");
+
 // merninstagram
 module.exports = {
     MONGOURI: process.env.MONGOURI,
-    JWT_SECRET: process.env.JWT_SECRET
+    JWT_SECRET: process.env.JWT_SECRET,
+    SENDGRID_APIKEY: process.env.SENDGRID_APIKEY
 }
