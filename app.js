435f8591bcbac1febe7295e98ff161605144ccb4
Bricx Carasco <bricxraincarasco21@gmail.com>
2020-07-03 10:59:13 +0800

    Trial and error deployment bugs and issues

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV == "production") {
     app.use(express.static('frontend/public'));
     const path = require('path');
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'public', 'index.html'));
+        res.sendFile(path.join(__dirname, 'frontend', 'public', 'index.html'));
     });
 }
 
