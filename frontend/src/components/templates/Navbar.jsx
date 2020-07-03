aaaadeee9599fc1381b1af901a4195d205d35f56
Bricx Carasco <bricxraincarasco21@gmail.com>
2020-07-03 16:50:36 +0800

    Implement search user function in navbar compenent

--- frontend/src/components/templates/Navbar.jsx
+++ frontend/src/components/templates/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useRef, useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import { UserContext } from '../../App';
 import M from 'materialize-css';
@@ -7,6 +7,10 @@ const NavbarT = () => {
     const history = useHistory();
 
     const { state, dispatch } = useContext(UserContext);
+    
+    const [ searchUser, setSearchUser ] = useState("");
+    const [ usersList, setUsersList ] = useState([]);
+    const searchModal = useRef(null);
 
     const logoutFunction = () => {
         localStorage.clear();
@@ -16,13 +20,49 @@ const NavbarT = () => {
         history.push('/signin');
     }
 
+    const resetSearchStrings = () => {
+        setSearchUser("");
+        setUsersList([]);
+    }
+
+    const closeModal = () => {
+        resetSearchStrings();
+        M.Modal.getInstance(searchModal.current).close();
+    }
+
+    const fetchUsers = (query) => {
+        setSearchUser(query);
+        if (query !== "") {
+            fetch('/search-user', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${localStorage.getItem("jwt")}`
+                },
+                body: JSON.stringify({
+                    query
+                })
+            })
+            .then(response => response.json())
+            .then(result => {
+                setUsersList(result.users);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+        } else {
+            resetSearchStrings();
+        }
+    }
+
     const renderList = () => {
         if (state) {
             return [
-                <li key="0"><Link to="/following">Posts</Link></li>,
-                <li key="1" ><Link to="/profile">Profile</Link></li>,
-                <li key="2"><Link to="/create">Create Post</Link></li>,
-                <li key="3">
+                <li key="0"><a href="false"><i data-target="search-modal" className="modal-trigger large material-icons navbar-search">search</i></a></li>,
+                <li key="1"><Link to="/following">Posts</Link></li>,
+                <li key="2" ><Link to="/profile">Profile</Link></li>,
+                <li key="3"><Link to="/create">Create Post</Link></li>,
+                <li key="4">
                     <button 
                     className="btn #c62828 red darken-3"
                     onClick={() => logoutFunction()}>Logout</button>
@@ -30,8 +70,8 @@ const NavbarT = () => {
             ];
         } else {
             return [
-                <li key="4"><Link to="/signin">Signin</Link></li>,
-                <li key="5"><Link to="/signup">Signup</Link></li>
+                <li key="5"><Link to="/signin">Signin</Link></li>,
+                <li key="6"><Link to="/signup">Signup</Link></li>
             ];
         }
     }
@@ -39,10 +79,11 @@ const NavbarT = () => {
     const renderListMobile = () => {
         if (state) {
             return [
-                <li key="0"><a href="/following">Posts</a></li>,
-                <li key="1" ><a href="/profile">Profile</a></li>,
-                <li key="2"><a href="/create">Create Post</a></li>,
-                <li key="3">
+                <li key="0"><a href="false"><i data-target="search-modal" className="modal-trigger large material-icons navbar-search">search</i></a></li>,
+                <li key="1"><a href="/following">Posts</a></li>,
+                <li key="2" ><a href="/profile">Profile</a></li>,
+                <li key="3"><a href="/create">Create Post</a></li>,
+                <li key="4">
                     <button 
                     className="btn #c62828 red darken-3 btn-logout"
                     onClick={() => logoutFunction()}>Logout</button>
@@ -50,11 +91,21 @@ const NavbarT = () => {
             ];
         } else {
             return [
-                <li key="4"><Link to="/signin">Signin</Link></li>,
-                <li key="5"><Link to="/signup">Signup</Link></li>
+                <li key="5"><Link to="/signin">Signin</Link></li>,
+                <li key="6"><Link to="/signup">Signup</Link></li>
             ];
         }
     }
+
+    useEffect(() => {
+        M.Modal.init(searchModal.current);
+    }, [searchModal]);
+
+    useEffect(() => {
+        if (searchUser === "") {
+            resetSearchStrings();
+        }
+    }, [searchUser]);
 
     return (
         <>
@@ -74,6 +125,43 @@ const NavbarT = () => {
                     </div>
                 </div>
             </nav>
+
+            <div id="search-modal" className="modal" ref={searchModal}>
+                <div className="modal-content">
+                    <input 
+                        type="text" 
+                        placeholder="Search user"
+                        value={searchUser}
+                        onChange={(event) => fetchUsers(event.target.value)}
+                        />
+
+                        {
+                            usersList.length !== 0 || searchUser !== "" ?
+                            <ul className="collection">
+                                {
+                                    usersList.map(user => {
+                                        return <li 
+                                            key={user._id} 
+                                            className="collection-item">
+                                                <Link 
+                                                    to={ state._id === user._id ? `/profile` : `/profile/${user._id}`} 
+                                                    onClick={closeModal}>
+                                                        <b>{user.name}</b> <i>{user.email}</i>
+                                                </Link>
+                                            </li>
+                                    })
+                                }
+                            </ul>
+                            : <h5 className="center"><i>Search users</i></h5>
+                        }
+                </div>
+                <div className="modal-footer">
+                    <button 
+                        className="modal-close waves-effect waves-green btn-flat" 
+                        onClick={resetSearchStrings}>
+                            Close</button>
+                </div>
+            </div>
 
             <ul className="sidenav" id="mobile-demo">
                 {
