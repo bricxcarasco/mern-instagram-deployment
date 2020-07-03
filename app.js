87cbc8b8bf1081fbdbce166bd2b344565c7caad0
Bricx Carasco <bricxraincarasco21@gmail.com>
2020-07-03 10:50:38 +0800

    Trial and error deployment bugs and issues

--- app.js
+++ app.js
@@ -33,10 +33,10 @@ app.use(require('./routes/post'));
 console.log(process.env.NODE_ENV);
 
 if (process.env.NODE_ENV == "production") {
-    app.use(express.static('frontend/build'));
+    app.use(express.static('frontend/public'));
     const path = require('path');
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'frontend', 'public', 'index.html'));
     });
 }
 
